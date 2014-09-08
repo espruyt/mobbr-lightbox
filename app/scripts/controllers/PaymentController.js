115beaf396c1cd4538486b8258db6a670e5d9d5a
Han Dijk <info@handijk.nl>
2014-09-08 17:04:53 +0200

    First lightbox styling, #1

--- app/scripts/controllers/PaymentController.js
+++ app/scripts/controllers/PaymentController.js
@@ -1,5 +1,5 @@
 angular.module('mobbr-lightbox.controllers')
-    .controller('PaymentController', function ($scope, $location, $routeParams, MobbrPayment, MobbrPerson, MobbrBalance, MobbrUri, MobbrUser, mobbrSession) {
+    .controller('PaymentController', function ($scope, $location, $state, MobbrPayment, MobbrPerson, MobbrBalance, MobbrUri, MobbrUser, mobbrSession) {
         'use strict';
 
         $scope.form = {};
@@ -21,7 +21,7 @@ angular.module('mobbr-lightbox.controllers')
             }
         }
 
-        var url = window.atob($routeParams.hash);
+        var url = window.atob($state.params.hash);
         $scope.task = MobbrUri.info({ url: url }, function (response) {
 
             if (response.result.script && response.result.script.url && response.result.script.url !== url) {
@@ -45,12 +45,14 @@ angular.module('mobbr-lightbox.controllers')
         $scope.preview = function (showPreview, callBack) {
             $scope.showPreview = false;
             var currency = $scope.currency && $scope.currency.currency_iso || $scope.currency;
-            if ($scope.form.amount && currency) {
+            console.log(currency, $scope.amount);
+            if ($scope.amount && currency) {
+                console.log('ladun');
                 $scope.previewLoading = MobbrPayment.preview({
                     data: $scope.url,
                     currency: currency,
-                    amount: $scope.form.amount,
-                    invoiced: $scope.form.wantInvoices
+                    amount: $scope.amount,
+                    invoiced: $scope.wantInvoices
                 }, function (response) {
                     if (callBack) {
                         callBack(response.result.hash);
@@ -65,21 +67,23 @@ angular.module('mobbr-lightbox.controllers')
             $scope.preview(false, confirm);
         }
 
-        $scope.login = function () {
-            $scope.authenticating = MobbrUser.passwordLogin({ username: $scope.loginform.username, password: $scope.loginform.password }, handleMessage, handleMessage);
+        $scope.login = function (username, password) {
+            $scope.authenticating = MobbrUser.passwordLogin({ username: username, password: password }, handleMessage, handleMessage).then(function () {
+                $state.go('payment');
+            });
         };
 
 
         $scope.performPayment = function () {
             $scope.performing = true;
             if ($scope.formHolder.pledgeForm && $scope.formHolder.pledgeForm.$valid) {
-                if (mobbrSession.isAuthorized()) {
+                //if (mobbrSession.isAuthorized()) {
                     perform();
-                } else {
-                    $scope.authenticating = MobbrUser.passwordLogin({ username: $scope.loginform.username, password: $scope.loginform.password }, function () {
-                        perform();
-                    }, handleMessage);
-                }
+                //} else {
+                //    $scope.authenticating = MobbrUser.passwordLogin({ username: $scope.loginform.username, password: $scope.loginform.password }, function () {
+                //        perform();
+                //    }, handleMessage);
+               // }
             }
         };
 
